745ff9965e382ab6c9c88faa85e32d4bac7ba522
Amh <abaams78@gmail.com>
2020-04-07 14:32:59 -0500

    insertion sort implementation

--- src/main/java/com/udemy/sort/InsertionSort.java
+++ src/main/java/com/udemy/sort/InsertionSort.java
@@ -10,10 +10,22 @@ public class InsertionSort {
     }
 
     /**
+     * Start from the left side of the array and increase the sorted size by sorting and adding the left element from
+     * the unsorted partition by finding the insertion position. When traversing in the sorted section if we found
+     * element greater than that element all elements right side are grater  because it is already sorted
      *
+     * increase sorted partion from 1 to array length
      * @param array array containing the elements to be sorted
      */
     private static void insertionSort(int[] array){
+        for (int firstUnsorted = 1; firstUnsorted < array.length; firstUnsorted++){
+            int newElement = array[firstUnsorted];
+            int i;
 
+            for (i = firstUnsorted; i > 0 && array[i-1] > newElement; i--){
+                array[i] = array[i-1];
+            }
+            array[i] = newElement;
+        }
     }
 }
