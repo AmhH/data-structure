52ebc61690ae97c39498e1c73a73d60bf794faa4
Amh <abaams78@gmail.com>
2020-04-07 12:09:58 -0500

    sort started

--- src/main/java/com/udemy/arrays/TestArrays.java
+++ src/main/java/com/udemy/arrays/TestArrays.java
@@ -1,4 +1,11 @@
 package com.udemy.arrays;
 
+import java.util.Arrays;
+
 public class TestArrays {
+
+    public static void main(String[] args) {
+        int[] intArray = new int[7];
+        System.out.println(Arrays.toString(intArray));
+    }
 }
