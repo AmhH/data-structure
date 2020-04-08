6b9760d3dbe54e3563d3d62c845ef21ff444eecc
Amh <abaams78@gmail.com>
2020-04-08 13:36:03 -0500

    merge sort done

--- src/main/java/com/udemy/sort/MergeSort.java
+++ src/main/java/com/udemy/sort/MergeSort.java
@@ -1,9 +1,13 @@
 package com.udemy.sort;
 
+import java.util.Arrays;
+
 public class MergeSort {
 
     public static void main(String[] args) {
         int[] intArray = {20, 35, -15, 7, 55, 1, -22};
+        mergeSort(intArray, 0, intArray.length);
+        System.out.println(Arrays.toString(intArray));
 
     }
     /**
@@ -22,6 +26,31 @@ public class MergeSort {
         int mid = (start + end) / 2;
         mergeSort(array, start, mid);
         mergeSort(array, mid, end);
-        //merge(array, start, mid, end);
+        merge(array, start, mid, end);
+    }
+
+    private static void merge(int[] array, int start, int mid, int end) {
+        if(array[mid-1] <= array[mid]){
+            return;
+        }
+
+        int i = start;
+        int j = mid;
+        int tempIndex = 0;
+        int[] temp = new int[end - start];
+
+        while (i < mid && j < end){
+            temp[tempIndex++] = array[i] <= array[j] ? array[i++] : array[j++];
+        }
+
+        /**
+         * if there are elements left in teh right array no need to copy if do it will be overriding.
+         *         We are always copying the smallest elements
+         *         {32, 34} {33, 36}
+         *         {32, 33, 34}
+         */
+        System.arraycopy(array, i, array, start + tempIndex, mid -i);
+        System.arraycopy(temp, 0, array, start, tempIndex);
+
     }
 }
