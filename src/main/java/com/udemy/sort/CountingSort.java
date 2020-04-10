8ad164a69231898cee019c61f27e66b394928a21
Amh <abaams78@gmail.com>
2020-04-10 14:52:41 -0500

    counting sort

---
+++ src/main/java/com/udemy/sort/CountingSort.java
@@ -0,0 +1,16 @@
+package com.udemy.sort;
+
+public class CountingSort {
+
+    public static void main(String[] args) {
+        int[] intArray = {2, 5, 9, 8, 2, 8, 7, 10, 4, 3};
+    }
+
+    /**
+     * Makes assumptions about data, non-negative discrete values only
+     * @param array array to be sorted
+     */
+    public static void countingSort(int[] array){
+
+    }
+}
