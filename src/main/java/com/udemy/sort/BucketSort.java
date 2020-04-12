0300277f8a4df2a94578fcb4ac5975c594f63364
Amh <abaams78@gmail.com>
2020-04-12 13:11:57 -0500

    bucket sort and hashing

---
+++ src/main/java/com/udemy/sort/BucketSort.java
@@ -0,0 +1,47 @@
+package com.udemy.sort;
+
+import java.util.*;
+
+
+/**
+ * Uses hashing. Distributes the items into buckets (Scattering) -> sort items in each bucket -> merge bucket (Gathering)
+ * Values in bucket X should be greater than values in X-1 and less than values in X+1
+ */
+public class BucketSort {
+
+    public static void main(String[] args) {
+        int[] intArray = {54, 46, 83, 66,95, 92, 43};
+        bucketSort(intArray);
+
+        System.out.println(Arrays.toString(intArray));
+    }
+
+    private static void bucketSort(int[] intArray) {
+        List<Integer>[] buckets = new List[10];
+
+        for (int i = 0; i < buckets.length; i++) {
+            buckets[i] = new LinkedList<>();
+        }
+
+        for (int i = 0; i < intArray.length; i++) {
+            buckets[hash(intArray[i])].add(intArray[i]);
+        }
+
+        for (List list : buckets) {
+            Collections.sort(list);
+        }
+
+        int j = 0;
+        for (int i = 0; i < buckets.length; i++) {
+            for (int value : buckets[i]){
+                intArray[j++] = value;
+            }
+        }
+
+
+    }
+
+    private static int hash(int value){
+        return value / 10;
+    }
+}
